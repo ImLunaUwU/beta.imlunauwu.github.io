1506d6d2d2684d0527676e8c97e9a7f11f009644
Luna <luna@lunauwu.net>
2023-08-20 17:32:46 +0200

    1st Commit (BETA)

---
+++ assets/js/loadingscript.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+	setTimeout(function(){
+    document.getElementById("loading").id = 'loaded';
+	document.getElementById("bodyloading").id = 'bodyloaded';
+    console.log("Loading Complete!");
+    }, 30);
+
+	// Remove Overlay After Load
+	setTimeout(function(){
+    var el = document.getElementById('finishedloading');
+	el.remove();
+
+	// Fade-in body when the page has loaded
+	$("body").fadeIn(2000);
+	}, 1100);
+
+	// Remove Load Script After Load
+	setTimeout(function(){
+    var el = document.getElementById('selfdelete');
+	el.remove();
+	}, 1200);
+}
